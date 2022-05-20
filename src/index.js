59b66d5d4ebeb76704b5e2b6772a19b045ff78b5
rajaexz <deepkexz@gmail.com>
2022-05-20 17:58:55 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import SearchIcon from "./search.svg";
+import { useState, useEffect } from "react";
+import "./App.css";
+import MoiveComp from "./MovieComp";
+
+
+//make component
+// movie api :http://www.omdbapi.com/?i=tt3896198&apikey=549112d4
+
+const API_URL = 'https://www.omdbapi.com/?apikey=549112d4';
+
+const App = () => {
+    // const [count , setcount] = useState(1);
+    //next useState we are using \
+    //  var raja = count;
+    
+    const [movies,setMovie] = useState([]);
+    //fatch movie api data
+    const [searchTeam,setsearchTeam] = useState('');
+    const search = async (title) => {
+        const respons = await fetch(`${API_URL}&s=${title}`);
+        const data = await respons.json()
+     setMovie(data.Search);
+    }
+  
+    useEffect(() => {
+        search('new');
+    }, [])
+ 
+    return (
+        <div className="app">
+          <h1>MovieLand</h1>
+    
+          <div className="search">
+            <input
+            value={searchTeam}
+            
+             onChange={(e)=>{setsearchTeam(e.target.value) 
+            
+            }}
+              placeholder="Search for movies"
+            />
+            <img
+              src={SearchIcon}
+              alt="search"
+              onClick={()=>search(searchTeam)}
+            />
+          </div>
+       
+          {movies.length > 0 ? (
+            <div className="container">
+              {movies.map((movie) => (
+                <MoiveComp movie={movie} />
+              ))}
+            </div>
+          ) : (
+            <div className="empty">
+              <h2>No movies found</h2>
+            </div>
+          )}
+        </div>
+      );
+    };
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector('#root')
+)
